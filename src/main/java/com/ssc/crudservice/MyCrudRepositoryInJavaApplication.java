07d34335c1d22cb7c2b63a8419ab4cdf23358545
Pradeep <pradeep.d80@gmail.com>
2022-07-28 13:17:29 +0530

    Initial commit

---
+++ src/main/java/com/ssc/crudservice/MyCrudRepositoryInJavaApplication.java
@@ -0,0 +1,14 @@
+package com.ssc.crudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudRepositoryInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrudRepositoryInJavaApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/ssc/crudservice/myCRUDRepositoryInJava/MyCrudRepositoryInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssc.crudservice.myCRUDRepositoryInJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrudRepositoryInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
